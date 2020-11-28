d8346e82eb1979147a01bba3a2546308c90fbff0
Klimek <klimkowskib@gmail.com>
2020-11-28 17:37:20 +0100

    refactor get_authors func

--- book_library_app/authors/authors.py
+++ book_library_app/authors/authors.py
@@ -1,17 +1,20 @@
-from flask import jsonify, request
+from flask import jsonify
+from webargs.flaskparser import use_args
+
 from book_library_app import db
 from book_library_app.models import Author, AuthorSchema, author_schema
-from webargs.flaskparser import use_args
-from book_library_app.utils import validate_json_content_type
+from book_library_app.utils import validate_json_content_type, get_schema_args, apply_order, apply_filter, get_pagination
 from book_library_app.authors import authors_bp
+
+
 
 @authors_bp.route('/authors', methods=['GET'])
 def get_authors():
     query = Author.query
-    schema_args = Author.get_schema_args(request.args.get('fields'))
-    query = Author.apply_order(query, request.args.get('sort'))
-    query = Author.apply_filter(query)
-    items, pagination = Author.get_pagination(query)
+    schema_args = get_schema_args(Author)
+    query = apply_order(Author, query)
+    query = apply_filter(Author, query)
+    items, pagination = get_pagination(query, 'authors.get_authors')
     authors = AuthorSchema(**schema_args).dump(items)
 
     return jsonify({
--- book_library_app/models.py
+++ book_library_app/models.py
@@ -1,17 +1,6 @@
 from book_library_app import db
 from marshmallow import Schema, fields, validate, validates, ValidationError
-from datetime import datetime
-from flask_sqlalchemy import BaseQuery
-from sqlalchemy.orm.attributes import InstrumentedAttribute
-from sqlalchemy.sql.expression import BinaryExpression
-import re
-from flask import request, url_for
-from book_library_app import Config
-from typing import Tuple
-
-
-COMPARISON_OPERATORS_RE = re.compile(r'(.*)\[(gte|gt|lte|lt)\]')
-
+from datetime import datetime, date
 
 
 
@@ -27,74 +16,13 @@ class Author(db.Model):
         return f'<{self.__class__.__name__}>:{self.first_name} {self.last_name}'
 
     @staticmethod
-    def get_schema_args(fields: str) -> dict:
-        schema_args = {'many':True}
-        if fields:
-            schema_args['only'] = [field for field in fields.split(',') if field in Author.__table__.columns]
-        return schema_args
-
-    @staticmethod
-    def apply_order(query: BaseQuery, sort_keys:str) -> BaseQuery:
-        if sort_keys:
-            for key in sort_keys.split(','):
-                descending = False
-                if key.startswith(('-')):
-                    key = key[1:]
-                    descending = True
-                column_attr = getattr(Author, key, None)
-                if column_attr is not None:
-                    query = query.order_by(column_attr.desc()) if descending else query.order_by(column_attr)
-        return query
-
-    @staticmethod
-    def get_filter_argument(column_name: InstrumentedAttribute, value:str, operator:str) -> BinaryExpression:
-        operator_mapping = {
-            '==' : column_name == value,
-            'gte' : column_name >= value,
-            'gt' : column_name > value,
-            'lte' : column_name <= value,
-            'lt' : column_name < value
-        }
-        return operator_mapping[operator]
-
-
-    @staticmethod
-    def apply_filter(query: BaseQuery) -> BaseQuery:
-        for param, value in request.args.items():
-            if param not in {'fields', 'sort', 'page', 'limit'}:
-                operator = '=='
-                match = COMPARISON_OPERATORS_RE.match(param)
-                if match is not None:
-                    param, operator = match.groups()
-
-                column_attr = getattr(Author, param, None)
-                if column_attr is not None:
-                    if param == 'birth_date':
-                        try:
-                            value = datetime.strptime(value, '%d-%m-%Y').date()
-                        except ValueError:
-                            continue
-                    filter_argument = Author.get_filter_argument(column_attr, value, operator)
-                    query = query.filter(filter_argument)
-        return query
-
-    @staticmethod
-    def get_pagination(query: BaseQuery) -> Tuple[list, dict]:
-        page = request.args.get('page', 1, type=int)
-        limit = request.args.get('limit', Config.PER_PAGE, type=int)
-        params = {key:value for key,value in request.args.items() if key != 'page'}
-        paginate_obj = query.paginate(page, limit, False)
-        pagination = {
-            'total_pages' : paginate_obj.pages,
-            'total_records': paginate_obj.total,
-            'current_page' : url_for('authors.get_authors', page=page, **params)
-        }
-        if paginate_obj.has_next:
-            pagination['next_page'] = url_for('authors.get_authors', page=page+1, **params)
-
-        if paginate_obj.has_prev:
-            pagination['previous_page'] = url_for('authors.get_authors', page=page-1, **params)
-        return paginate_obj.items, pagination
+    def additional_validation(param:str, value:str) -> date:
+        if param == 'birth_date':
+            try:
+                value = datetime.strptime(value, '%d-%m-%Y').date()
+            except ValueError:
+                value = None
+        return value
 
 
 
