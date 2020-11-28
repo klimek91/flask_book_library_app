7eb94a5a518fa66cf3eed3ebf452578152c62a27
Klimek <klimkowskib@gmail.com>
2020-11-28 15:47:47 +0100

    update db-manage with books

--- book_library_app/commands/db_manage_commands.py
+++ book_library_app/commands/db_manage_commands.py
@@ -2,8 +2,16 @@ import json
 from pathlib import Path
 from datetime import datetime
 from book_library_app import db
-from book_library_app.models import Author
+from book_library_app.models import Author, Book
 from book_library_app.commands import db_manage_bp
+
+
+
+def load_json_data(file_name:str) -> list:
+    json_path = Path(__file__).parent.parent / 'samples' / file_name
+    with open(json_path) as file:
+        data_json = json.load(file)
+    return data_json
 
 
 @db_manage_bp.cli.group()
@@ -15,13 +23,18 @@ def db_manage():
 def add_data():
     """Add sample data to database"""
     try:
-        authors_path = Path(__file__).parent.parent / 'samples' / 'authors.json'
-        with open(authors_path) as file:
-            data_json = json.load(file)
+        data_json = load_json_data('authors.json')
         for item in data_json:
             item['birth_date'] = datetime.strptime(item['birth_date'], '%d-%m-%Y').date()
             author = Author(**item)
             db.session.add(author)
+
+        data_json = load_json_data('books.json')
+        for item in data_json:
+            book = Book(**item)
+            db.session.add(book)
+
+
             db.session.commit()
             print('Data has been succesfully added to database')
     except Exception as exc:
@@ -32,7 +45,10 @@ def add_data():
 def remove_data():
     """Remove all data from the database"""
     try:
-        db.session.execute('TRUNCATE TABLE authors')
+        db.session.execute('DELETE FROM books')
+        db.session.execute('ALTER TABLE books AUTO_INCREMENT = 1')
+        db.session.execute('DELETE FROM authors')
+        db.session.execute('ALTER TABLE authors AUTO_INCREMENT = 1')
         db.session.commit()
         print("Data has been successfully removed from database")
     except Exception as exc:
